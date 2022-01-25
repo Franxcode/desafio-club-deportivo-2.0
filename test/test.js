ae5e231d22e1e95212d2a20d21e723f41ebb2257
Francisco Oliva <francisco.j.oliva@gmail.com>
2022-01-25 12:22:13 -0300

    Challenge completed.

--- test/test.js
+++ test/test.js
@@ -1,21 +1,24 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const main = require('../index');
-	
+const main = require('..');
+const url = 'http://localhost:3000';
+
 chai.use(chaiHttp);
 
 describe('Probando API REST con Mocha - Chai', () => {
-    it('Probando ruta /deportes (GET)', () => {
-        chai.request(main)
-            .get('/deportes');
+    it('Probando ruta /deportes (GET)', (done) => {
+        chai.request(url)
+            .get('/deportes')
+            done();
     });
-    it('La data debe contener un JSON con la propiedad "deportes" y esta debe ser un arreglo.', () => {
-        chai.request(main)
+    it('La data debe contener un JSON con la propiedad "deportes" y esta debe ser un arreglo.', (done) => {
+            chai.request(url)
             .get('/deportes')
             .end((err, res) => {
-                let data = JSON.parse(res);
+                let data = JSON.parse(res.text);
                 chai.expect(data).to.have.property('deportes');
                 chai.expect(data.deportes).to.be.an('array');
+                done();
             });
     });
 });
